e743145a8d51b46f0dba6f5fbbfa7a69d6ee6b76
Rahul Bera <rahul.bera@safari.ethz.ch>
2025-09-15 02:07:17 +0200

    Increased deadlock cycle limit

--- src/champsim.cc
+++ src/champsim.cc
@@ -29,7 +29,7 @@
 #include "phase_info.h"
 #include "tracereader.h"
 
-constexpr int DEADLOCK_CYCLE{500};
+constexpr int DEADLOCK_CYCLE{500000};
 
 const auto start_time = std::chrono::steady_clock::now();
 
